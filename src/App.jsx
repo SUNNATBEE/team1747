4dd055b756fd47e4484693c973d633af09a6163d
SARDORALOYEV <aloyevsardor30@gmail.com>
2025-07-07 10:49:58 +0500

    GOJO JONKA QOSHDIM

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ const App = () => {
   return (
     <div>
       Salom bolalar
+
+
+
+
+          <h1 class="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-gray-400 via-yellow-500 to-orange-500 drop-shadow-lg">
+  GOJO JONKA
+</h1>
+
     </div>
   )
 }
